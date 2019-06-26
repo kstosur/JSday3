b73c8dfe727534126c268df3721b5c8347511c07
unknown <kstosur@wp.pl>
2019-06-26 14:20:51 +0200

    introduction velocity

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let position = 0
 const body = document.querySelector('body')
 const carElement = document.createElement('div')
+const tickDuration = 10
+const velocity = 100
+let position = 0
+
+function applyStylesToCar(){
+carElement.className = 'car-game__car-element'
 
 carElement.style.width = '100px'
 carElement.style.height = '100px'
@@ -11,10 +16,16 @@ carElement.style.left = '0px'
 
 body.appendChild(carElement)
 
+}
+function move(){
+    const displacementPerTick = velocity * (tickDuration / 1000)
+    position = position + displacementPerTick
+    carElement.style.left = position +'px'
+}
+
+applyStylesToCar()
+
 setInterval(
-    function(){
-        position = position + 10
-        carElement.style.left = position +'px'
-    },
-    100
+    move, // przekazujemy całą funkcję a nie wywołujemy 
+    tickDuration
 )
