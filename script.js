e66a0094f548c38c08c80c97eb1a22405d298c6c
unknown <kstosur@wp.pl>
2019-06-27 09:38:08 +0200

    added road

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const body = document.querySelector('body')
-const carElement = document.createElement('div')
+const containerElement = document.querySelector('body')
+const roadElement = document.createElement('div') // droga
+const carElement = document.createElement('div') //samochód 
 const tickDuration = 10
 const maxAcceleration = 10
 let velocity = 0
 let position = 0
 let acceleration = 0
 
-function applyStylesToCar(){
-carElement.className = 'car-game__car-element'
+function applyStylesToCarAndRoad() { // funkcja która rysuje samochód i droge
+    carElement.className = 'car-game__car-element'
+// samochód
+    carElement.style.width = '100px'
+    carElement.style.height = '100px'
+    carElement.style.backgroundColor = 'red'
 
-carElement.style.width = '100px'
-carElement.style.height = '100px'
-carElement.style.backgroundColor = 'red'
+    carElement.style.position = 'absolute'
+    carElement.style.left = '0px'
+// droga
+    roadElement.style.width = '100%'
+    roadElement.style.height = '100px'
+    roadElement.style.position = 'relative'
+    roadElement.style.backgroundColor = 'gray'
 
-carElement.style.position = 'absolute'
-carElement.style.left = '0px'
-
-body.appendChild(carElement)
+    roadElement.appendChild(carElement)
+    containerElement.appendChild(roadElement)
 
 }
-function move(){
+function move() {
     const time = tickDuration / 1000
     position = position + velocity * time + (acceleration * time * time) / 2
     velocity = velocity + acceleration * time
-    carElement.style.left = position +'px'
+    carElement.style.left = position + 'px'
 }
 
-applyStylesToCar()
+applyStylesToCarAndRoad()
 
 setInterval(
     move, // przekazujemy całą funkcję a nie wywołujemy 
@@ -34,23 +41,23 @@ setInterval(
 )
 window.addEventListener(
     'keydown',
-    function (event){
-        if (event.key === 'a'){
-           acceleration = maxAcceleration
+    function (event) {
+        if (event.key === 'a') {
+            acceleration = maxAcceleration
         }
-        if (event.key === 'b'){
+        if (event.key === 'b') {
             acceleration = -maxAcceleration
-         }
+        }
     }
 )
 window.addEventListener(
     'keyup',
-    function (event){
-        if (event.key === 'a'){
-           acceleration = 0
-        }
-        if (event.key === 'b'){
+    function (event) {
+        if (event.key === 'a') {
             acceleration = 0
-         }
+        }
+        if (event.key === 'b') {
+            acceleration = 0
+        }
     }
 )
